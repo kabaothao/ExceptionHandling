fbfda1260e31bc47739919419baab1ddd933112f
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-15 09:49:45 -0500

    updated codes and notes

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace ExceptionHandling // Note: actual namespace depends on the project name.
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            var calculator = new Calculator();
-            var result = calculator.Divide(5, 0);
 
+            try
+            {
+                var calculator = new Calculator();
+                var result = calculator.Divide(5, 0);
+
+            }
+            catch (Exception)
+            {
+
+                
+                Console.WriteLine("Sorry, an unexpected error occured.");
+            }
+            
+            
+           
 
         }
     }
 }
+
+
+/*
+ you should always have a global exception handling block in your application.
+
+ */
