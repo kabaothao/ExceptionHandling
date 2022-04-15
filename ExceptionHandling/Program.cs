e04b47df9df3e6dff0510b427d2af3a1987473b5
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-15 10:02:33 -0500

    updated codes and notes

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -13,15 +13,16 @@ namespace ExceptionHandling // Note: actual namespace depends on the project nam
                 var result = calculator.Divide(5, 0);
 
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
-                
-                Console.WriteLine("Sorry, an unexpected error occured.");
+                Console.WriteLine("You cannot divide by 0.");
             }
-            
-            
-           
+            finally
+            {
+                Console.WriteLine();
+            }
+
+
 
         }
     }
@@ -30,5 +31,30 @@ namespace ExceptionHandling // Note: actual namespace depends on the project nam
 
 /*
  you should always have a global exception handling block in your application.
+
+
+Target site is the method where the exception happened.
+
+So these four properties are really common when working with exceptions.
+
+Inner exception represents an exception that could be inside this exception.
+
+
+
+So why do we need finally block?
+
+In.NET, we have classes that access unmanaged resources.
+
+What do I mean by unmanaged resources,
+
+that is resources that are not managed by CLR,
+
+so there is no garbage collection applied to them.
+
+Examples are file handles,
+
+database connections, network connections,
+
+graphic handles and in situations like that we need to manually do the cleanup.
 
  */
