487e331a3d04597781ddaf9b00f25a48ff16cd18
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-15 09:43:42 -0500

    updated code

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,2 +1,15 @@
-﻿hub.co// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            var result = calculator.Divide(5, 0);
+
+
+        }
+    }
+}
